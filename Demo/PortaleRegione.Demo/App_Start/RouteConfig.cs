db288a74b24f3274241a52ce98f454582f30c6a1
matteocattapan <matteo.cattapan@gamsoft.it>
2023-11-23 11:49:22 +0000

    update demo project

---
+++ Demo/PortaleRegione.Demo/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PortaleRegione.Demo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.LowercaseUrls = true;
+            
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new {controller = "Mockup", action = "Index", id = UrlParameter.Optional}
+            );
+        }
+    }
+}
