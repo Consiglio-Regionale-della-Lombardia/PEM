6fd516f81badccc1186816383c760a80a6744db9
matteocattapan <matteocattapan@gmail.com>
2024-05-03 09:29:39 +0100

    #938 fix

--- Sorgenti API/PortaleRegione.API/PortaleRegione.API/Properties/AssemblyInfo.cs
+++ Sorgenti API/PortaleRegione.API/PortaleRegione.API/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // È possibile specificare tutti i valori o lasciare i valori predefiniti per Revisione e Numeri build
 // utilizzando l'asterisco (*) come illustrato di seguito:
 [assembly: AssemblyVersion("2023.10.24")]
-[assembly: AssemblyFileVersion("2023.10.24.8")]
+[assembly: AssemblyFileVersion("2023.10.24.9")]
