0df84906cdaf447614f304e5679f59a478729db4
matteocattapan <matteocattapan@gmail.com>
2024-11-21 17:10:33 +0000

    #1093

--- Sorgenti API/PortaleRegione.DTO/Domain/AttoDASIReportDto.cs
+++ Sorgenti API/PortaleRegione.DTO/Domain/AttoDASIReportDto.cs
@@ -25,93 +25,67 @@ namespace PortaleRegione.DTO.Domain;
 
 public class AttoDASIReportDto
 {
-    [DisplayName("Tipo e numero atto")] public string Display { get; set; }
+    // L'ordinamento è stato fatto in base al ticket #1093
 
-    [DisplayName("Tipo e numero atto esteso")]
-    public string DisplayExtended { get; set; }
-
+    [DisplayName("Legislatura")] public int Legislatura { get; set; }
+    [DisplayName("Etichetta")] public string Etichetta { get; set; }
     [DisplayName("Tipo atto")] public int Tipo { get; set; }
     [DisplayName("Numero atto")] public string NAtto { get; set; }
+    [DisplayName("Tipo e numero atto")] public string Display { get; set; }
+    [DisplayName("Tipo e numero atto esteso")] public string DisplayExtended { get; set; }
     [DisplayName("Tipo mozione")] public int TipoMOZ { get; set; } = 0;
-    [DisplayName("Etichetta")] public string Etichetta { get; set; }
-
+    [DisplayName("Oggetto")] public string Oggetto { get; set; }
     [DisplayName("Data presentazione")] public DateTime Timestamp { get; set; }
-    [DisplayName("Data ritiro")] public DateTime? DataRitiro { get; set; }
     [DisplayName("Data annunzio")] public DateTime? DataAnnunzio { get; set; }
+    [DisplayName("Protocollo")] public string Protocollo { get; set; }
+    [DisplayName("Codice materia")] public string CodiceMateria { get; set; }
+    [DisplayName("Proponente")] public Guid? UIDPersonaProponente { get; set; }
+    [DisplayName("Firmatari")] public string Firme { get; set; }
+    [DisplayName("Conteggio firme")] public int ConteggioFirme { get; set; }
+    [DisplayName("Gruppo politico")] public int id_gruppo { get; set; }  // #1021
+    [DisplayName("Area politica")] public int AreaPolitica { get; set; }
+    [DisplayName("Commissioni proponenti RIS")] public List<OrganoDto> CommissioniProponenti { get; set; }
+    [DisplayName("Stato dell’atto")] public int IDStato { get; set; } // #1021
+    [DisplayName("Motivo chiusura iter")] public int? TipoChiusuraIter { get; set; } // #1021
+    [DisplayName("DCR/DCCR")] public int? DCR { get; set; }
+    [DisplayName("Data richiesta iscrizione in seduta")] public string DataRichiestaIscrizioneSeduta { get; set; }
+    [DisplayName("Data iscrizione in seduta")] public DateTime? DataIscrizioneSeduta { get; set; }
+    [DisplayName("Data seduta")] public Guid? UIDSeduta { get; set; }
     [DisplayName("Data chiusura")] public DateTime? DataChiusuraIter { get; set; }
     [DisplayName("Data comunicazione assemblea")] public DateTime? DataComunicazioneAssemblea { get; set; }
-
-    [DisplayName("Oggetto")] public string Oggetto { get; set; }
+    [DisplayName("Data ritiro")] public DateTime? DataRitiro { get; set; }
+    [DisplayName("Tipo votazione")] public int? TipoVotazioneIter { get; set; }
+    [DisplayName("Emendato")] public bool Emendato { get; set; }
+    [DisplayName("Abbinamenti")] public string Abbinamenti { get; set; }
+    [DisplayName("Note")] public string Note { get; set; }
+    [DisplayName("Allegati dell’atto")] public string Documenti { get; set; } // #1021
+    [DisplayName("Tipo risposta richiesta")] public int IDTipo_Risposta { get; set; }
+    [DisplayName("Tipo risposta fornita")] public int? IDTipo_Risposta_Effettiva { get; set; }
+    [DisplayName("Informazioni risposte")] public string Risposte { get; set; }
+    [DisplayName("Sollecito")] public bool Sollecito { get; set; }
+    [DisplayName("Iter multiplo")] public bool IterMultiplo { get; set; } = false;
+    [DisplayName("Non passaggio in esame")] public bool Non_Passaggio_In_Esame { get; set; } = false;
+    [DisplayName("Presentato oltre i termini")] public bool PresentatoOltreITermini { get; set; } = false;
+    [DisplayName("Pubblicato")] public bool Pubblicato { get; set; }
     [DisplayName("Premesse")] public string Premesse { get; set; }
     [DisplayName("Richiesta")] public string Richiesta { get; set; }
     [DisplayName("Impegni e scadenze")] public string ImpegniScadenze { get; set; } // #1021
     [DisplayName("Stato attuazione")] public string StatoAttuazione { get; set; }
     [DisplayName("Competenza monitoraggio")] public string CompetenzaMonitoraggio { get; set; }
-    [DisplayName("Firmatari")] public string Firme { get; set; }
-    [DisplayName("Conteggio firme")] public int ConteggioFirme { get; set; }
-    [DisplayName("Proponente")] public Guid? UIDPersonaProponente { get; set; }
-    [DisplayName("Commissioni proponenti RIS")] public List<OrganoDto> CommissioniProponenti { get; set; }
-
-    [DisplayName("Gruppo politico")] public int id_gruppo { get; set; }  // #1021
-    [DisplayName("Stato dell’atto")] public int IDStato { get; set; } // #1021
-    [DisplayName("Protocollo")] public string Protocollo { get; set; }
-    [DisplayName("Codice materia")] public string CodiceMateria { get; set; }
-
-    [DisplayName("Tipo risposta richiesta")]
-    public int IDTipo_Risposta { get; set; }
-
-    [DisplayName("Abbinamenti")] public string Abbinamenti { get; set; }
-    [DisplayName("Informazioni risposte")] public string Risposte { get; set; }
-    [DisplayName("Motivo chiusura iter")] public int? TipoChiusuraIter { get; set; } // #1021
-    [DisplayName("Tipo votazione")] public int? TipoVotazioneIter { get; set; }
-    [DisplayName("Emendato")] public bool Emendato { get; set; }
-    [DisplayName("Pubblicato")] public bool Pubblicato { get; set; }
-    [DisplayName("Sollecito")] public bool Sollecito { get; set; }
+    [DisplayName("Data trasmissione monitoraggio")] public DateTime? DataTrasmissioneMonitoraggio { get; set; }
     [DisplayName("Informazioni monitoraggio")] public string Monitoraggi { get; set; }
+    [DisplayName("Monitoraggio concluso")] public bool MonitoraggioConcluso { get; set; }
     [DisplayName("Area tematica")] public string AreaTematica { get; set; }
     [DisplayName("Altri soggetti")] public string AltriSoggetti { get; set; }
-    [DisplayName("Monitoraggio concluso")] public bool MonitoraggioConcluso { get; set; }
-    [DisplayName("Data trasmissione monitoraggio")] public DateTime? DataTrasmissioneMonitoraggio { get; set; }
-    [DisplayName("DCR/DCCR")] public int? DCR { get; set; }
-    [DisplayName("Data richiesta iscrizione in seduta")] public string DataRichiestaIscrizioneSeduta { get; set; }
-    [DisplayName("Data iscrizione in seduta")] public DateTime? DataIscrizioneSeduta { get; set; }
-    [DisplayName("Data seduta")] public Guid? UIDSeduta { get; set; }
-    [DisplayName("Area politica")] public int AreaPolitica { get; set; }
-    [DisplayName("Legislatura")] public int Legislatura { get; set; }
-    [DisplayName("Link pubblico")] public Guid UID_QRCode { get; set; }
-    [DisplayName("Non passaggio in esame")]
-    public bool Non_Passaggio_In_Esame { get; set; } = false;
-
-
-    [DisplayName("Privacy - dati giudiziari")]
-    public bool Privacy_Dati_Personali_Giudiziari { get; set; }
-
-    [DisplayName("Privacy - dati sanitari")]
-    public bool Privacy_Divieto_Pubblicazione_Salute { get; set; }
-
-    [DisplayName("Privacy - dati di natura sessuale")]
-    public bool Privacy_Divieto_Pubblicazione_Vita_Sessuale { get; set; }
-
-    [DisplayName("Privacy - divieto di pubblicazione")]
-    public bool Privacy_Divieto_Pubblicazione { get; set; }
-
-    [DisplayName("Privacy - dati sensibili")]
-    public bool Privacy_Dati_Personali_Sensibili { get; set; }
-
-    [DisplayName("Privicy - altri motivi")]
-    public bool Privacy_Divieto_Pubblicazione_Altri { get; set; }
-
-    [DisplayName("Privacy - dati personali semplici")]
-    public bool Privacy_Dati_Personali_Semplici { get; set; }
-
+    [DisplayName("Privacy - dati giudiziari")] public bool Privacy_Dati_Personali_Giudiziari { get; set; }
+    [DisplayName("Privacy - dati sanitari")] public bool Privacy_Divieto_Pubblicazione_Salute { get; set; }
+    [DisplayName("Privacy - dati di natura sessuale")] public bool Privacy_Divieto_Pubblicazione_Vita_Sessuale { get; set; }
+    [DisplayName("Privacy - divieto di pubblicazione")] public bool Privacy_Divieto_Pubblicazione { get; set; }
+    [DisplayName("Privacy - dati sensibili")] public bool Privacy_Dati_Personali_Sensibili { get; set; }
+    [DisplayName("Privicy - altri motivi")] public bool Privacy_Divieto_Pubblicazione_Altri { get; set; }
+    [DisplayName("Privacy - dati personali semplici")] public bool Privacy_Dati_Personali_Semplici { get; set; }
     [DisplayName("Privacy")] public bool Privacy { get; set; }
-    [DisplayName("Allegati dell’atto")] public string Documenti { get; set; } // #1021
-    [DisplayName("Note")] public string Note { get; set; }
-    [DisplayName("Iter multiplo")] public bool IterMultiplo { get; set; } = false;
-
-    [DisplayName("Presentato oltre i termini")] public bool PresentatoOltreITermini { get; set; } = false;
     public string BURL { get; set; }
-
-    [DisplayName("Tipo risposta fornita")] public int? IDTipo_Risposta_Effettiva { get; set; }
+    [DisplayName("Link pubblico")] public Guid UID_QRCode { get; set; }
     [DisplayName("UIDAtto")] public Guid UIDAtto { get; set; }
 }
