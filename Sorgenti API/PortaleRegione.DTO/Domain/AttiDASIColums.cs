fd4e712e0b49baef71f65b6b898bc277d866e5fa
matteocattapan <matteocattapan@gmail.com>
2024-11-21 17:10:14 +0000

    #989

--- Sorgenti API/PortaleRegione.DTO/Domain/AttiDASIColums.cs
+++ Sorgenti API/PortaleRegione.DTO/Domain/AttiDASIColums.cs
@@ -38,10 +38,13 @@ public class AttiDASIColums
     [DisplayName("Gruppo politico")] public int id_gruppo { get; set; }  // #1021
     [DisplayName("Firme ritirate")] public string Firme_ritirate{ get; set; } // #1048
     [DisplayName("Area politica")] public int AreaPolitica { get; set; }
+    [DisplayName("Protocollo")] public string Protocollo { get; set; }
     [DisplayName("Codice materia")] public string CodiceMateria { get; set; }
     [DisplayName("Non passaggio in esame")] public bool Non_Passaggio_In_Esame { get; set; } = false;
     [DisplayName("Informazioni risposte")] public string Risposte { get; set; }
     [DisplayName("Informazioni monitoraggio")] public string Monitoraggi { get; set; }
+    [DisplayName("Data trasmissione monitoraggio")] public DateTime? DataTrasmissioneMonitoraggio { get; set; }
+    [DisplayName("Monitoraggio concluso")] public bool MonitoraggioConcluso { get; set; }
     [DisplayName("Commissioni proponenti RIS")] public List<OrganoDto> CommissioniProponenti { get; set; }
     [DisplayName("Note")] public string Note { get; set; }
     [DisplayName("Data annunzio")] public DateTime? DataAnnunzio { get; set; }
@@ -50,40 +53,37 @@ public class AttiDASIColums
     [DisplayName("Motivo chiusura iter")] public int? TipoChiusuraIter { get; set; } // #1021
     [DisplayName("Data chiusura")] public DateTime? DataChiusuraIter { get; set; }
     [DisplayName("Data seduta proposta")] public string DataRichiestaIscrizioneSeduta { get; set; } // #1001
+    [DisplayName("Data iscrizione in seduta")] public DateTime? DataIscrizioneSeduta { get; set; }
     [DisplayName("Seduta")] public Guid? UIDSeduta { get; set; }
     [DisplayName("Tipo votazione")] public int? TipoVotazioneIter { get; set; }
     [DisplayName("Emendato")] public bool Emendato { get; set; }
     [DisplayName("DCR/DCCR")] public int? DCR { get; set; }
+    [DisplayName("Abbinamenti")] public string Abbinamenti { get; set; }
     [DisplayName("Impegni e scadenze")] public string ImpegniScadenze { get; set; } // #1021
     // Mancano le commissioni richieste (itl, itr, iqt)
     [DisplayName("Tipo risposta richiesta")] public int IDTipo_Risposta { get; set; }
     [DisplayName("Tipo risposta fornita")] public int? IDTipo_Risposta_Effettiva { get; set; }
     [DisplayName("Iter multiplo")] public bool IterMultiplo { get; set; } = false;
     // Manca il calcolo dei gg di ritardo
+    [DisplayName("Pubblicato")] public bool Pubblicato { get; set; }
     [DisplayName("Sollecito")] public bool Sollecito { get; set; }
     [DisplayName("Data trasmissione")] public DateTime? DataTrasmissione { get; set; }
     [DisplayName("Conteggio firme")] public int ConteggioFirme { get; set; }
     [DisplayName("Privacy")] public bool Privacy { get; set; }
     [DisplayName("Privacy - divieto di pubblicazione")] public bool Privacy_Divieto_Pubblicazione { get; set; }
     [DisplayName("Presentato oltre i termini")] public bool PresentatoOltreITermini { get; set; } = false;
+    [DisplayName("Allegati dell’atto")] public string Documenti { get; set; } // #1021
 
 
 
-
-    //[DisplayName("Data iscrizione in seduta")] public DateTime? DataIscrizioneSeduta { get; set; }
     //[DisplayName("Data ritiro")] public DateTime? DataRitiro { get; set; }
     //[DisplayName("Premesse")] public string Premesse { get; set; }
     //[DisplayName("Richiesta")] public string Richiesta { get; set; }
     //[DisplayName("Stato attuazione")] public string StatoAttuazione { get; set; }
     //[DisplayName("Competenza monitoraggio")] public string CompetenzaMonitoraggio { get; set; }
-    //[DisplayName("Protocollo")] public string Protocollo { get; set; }
-    //[DisplayName("Abbinamenti")] public string Abbinamenti { get; set; }
     //[DisplayName("Area tematica")] public string AreaTematica { get; set; }
     //[DisplayName("Altri soggetti")] public string AltriSoggetti { get; set; }
-    //[DisplayName("Monitoraggio concluso")] public bool MonitoraggioConcluso { get; set; }
-    //[DisplayName("Data trasmissione monitoraggio")] public DateTime? DataTrasmissioneMonitoraggio { get; set; }
-    //[DisplayName("Pubblicato")] public bool Pubblicato { get; set; }
-    //[DisplayName("Allegati dell’atto")] public string Documenti { get; set; } // #1021
+    
     //public string BURL { get; set; }
     //[DisplayName("UIDAtto")] public Guid UIDAtto { get; set; }
 }
