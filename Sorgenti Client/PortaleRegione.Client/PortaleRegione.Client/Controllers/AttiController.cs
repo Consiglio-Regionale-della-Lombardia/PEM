de186db98cc2b265c5ddb4c4eb346e5927aa1eb7
matteocattapan <matteocattapan@gmail.com>
2025-03-25 09:30:07 +0000

    fix pulsante scarica documento in atti

--- Sorgenti Client/PortaleRegione.Client/PortaleRegione.Client/Controllers/AttiController.cs
+++ Sorgenti Client/PortaleRegione.Client/PortaleRegione.Client/Controllers/AttiController.cs
@@ -25,6 +25,7 @@ using PortaleRegione.Gateway;
 using System;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using PortaleRegione.Logger;
 
 namespace PortaleRegione.Client.Controllers
 {
@@ -185,7 +186,7 @@ namespace PortaleRegione.Client.Controllers
         /// <param name="path">Percorso del documento atto</param>
         /// <returns></returns>
         [HttpGet]
-        [Route("file")]
+        [Route("scarica-file-atto")]
         public async Task<ActionResult> Download(string path)
         {
             try
@@ -197,7 +198,7 @@ namespace PortaleRegione.Client.Controllers
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                Log.Error("Download Atto", e);
                 return Json(new ErrorResponse(e.Message), JsonRequestBehavior.AllowGet);
             }
         }
